5b38c8df172a3d3a5a804c840f3a1e54fa3b9225
Lucas Henrique Rodrigues Pereira Rios <lucashrios95@gmail.com>
2026-01-08 09:51:58 -0300

    Fix: Congiguração de deploy na railway

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,8 +1,10 @@
 import { app } from "./app";
 import { startJobs } from "../jobs";
 
-app.listen(3333, () => {
-    console.log("Server Online");
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+    console.log(`Server Online on port ${PORT}`);
     startJobs();
 });
 
